40baa00a0490ec972cf789efc6c89b978c8ecae1
Saúl Valdelvira <saul@saulv.es>
2025-11-17 18:26:27 +0100

    Add shortcuts for opening urls

--- config.def.h
+++ config.def.h
@@ -231,6 +231,9 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", NULL };
+static char *copyurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -c", NULL };
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
@@ -247,6 +250,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_K,           kscrollup,      {.i = -1} },
 	{ TERMMOD,              XK_J,           kscrolldown,    {.i = -1} },
+        { TERMMOD,              XK_O,           externalpipe,   { .v = openurlcmd } },
+        { TERMMOD,              XK_L,           externalpipe,   { .v = copyurlcmd } },
+
 };
 
 /*
